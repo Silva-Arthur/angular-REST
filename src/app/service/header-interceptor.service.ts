f001a06111ac48c6c9b5ac68220529ba346b5a0e
Arthur <pereiras.oarthur@gmail.com>
2021-06-16 23:27:05 -0300

    Tratamento da msg de erro no angular

--- src/app/service/header-interceptor.service.ts
+++ src/app/service/header-interceptor.service.ts
@@ -1,11 +1,26 @@
 import { Injectable, NgModule } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
 import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeaderInterceptorService implements HttpInterceptor {
 
   constructor() { }
+
+  processaError(error: HttpErrorResponse) {
+    let errorMessage = 'Erro desconhecido';
+
+    if (error.error instanceof ErrorEvent) {
+      console.error(error.error);
+      errorMessage = 'Error: ' + error.error.error;
+    } else {
+      errorMessage = 'Codigo: ' + error.error.code + '\nMenssagem: ' + error.error.error;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (localStorage.getItem('token') !== null) {
@@ -16,7 +31,7 @@ export class HeaderInterceptorService implements HttpInterceptor {
 
       });
 
-      return next.handle(tokenRequest);
+      return next.handle(tokenRequest).pipe(catchError(this.processaError));
     } else {
       return next.handle(req);
     }
